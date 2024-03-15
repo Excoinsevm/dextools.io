e4bc0b4683a6aa7f3fbec4a63556c4c781d3114e
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2024-03-16 01:42:07 +0600

    LiquidityInfoRetriever class

--- liquidity.py
+++ liquidity.py
@@ -1,54 +1,57 @@
-# Retrieve liquidity information of a specific pool.
 import dotenv
 import os
 import requests
 import sqlite3
 import time
 
-dotenv.load_dotenv()
-API_KEY = os.getenv('API_KEY')
+class LiquidityInfoRetriever:
+    def __init__(self, delay=60):
+        dotenv.load_dotenv()
+        self.API_KEY = os.getenv('API_KEY')
+        self.headers = {
+            'accept': 'application/json',
+            'x-api-key': self.API_KEY,
+        }
+        self.connection = sqlite3.connect('database.db')
+        self.cursor = self.connection.cursor()
+        self.delay = delay
 
-headers = {
-    'accept': 'application/json',
-    'x-api-key': '',
-}
+    def fetch_distinct_addresses(self):
+        query = """
+            SELECT DISTINCT address 
+            FROM LiquidityPools 
+            WHERE creationTime >= datetime('now', '-1 day');
+        """
+        self.cursor.execute(query)
+        return [row[0] for row in self.cursor.fetchall()]
 
-headers['x-api-key']=API_KEY
-
-connection = sqlite3.connect('database.db')
-cursor = connection.cursor()
-
-# SQLite query to select distinct addresses from the LiquidityPools table
-query = "SELECT DISTINCT address FROM LiquidityPools WHERE creationTime >= datetime('now', '-1 day');"
-
-# Execute the query
-cursor.execute(query)
-
-# Fetch all the distinct addresses as a list
-distinct_addresses = [row[0] for row in cursor.fetchall()]
-
-def extract_liquidity_info(LiquidityPoolAddress):
-    response = requests.get(
-        f'https://public-api.dextools.io/trial/v2/pool/ether/{LiquidityPoolAddress}/liquidity',
-        headers=headers,
-    )
-    if response.status_code == 200:
+    def extract_liquidity_info(self, liquidity_pool_address):
+        url = f'https://public-api.dextools.io/trial/v2/pool/ether/{liquidity_pool_address}/liquidity'
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
             liquidity = response.json()['data']['liquidity']
-            cursor.execute(
-                    """
-                        REPLACE INTO LiquidityData (
-                        LiquidityPoolAddress,liquidity
-                        ) VALUES (?, ?)
-                    """, 
-                    (
-                        LiquidityPoolAddress,liquidity
-                    )
-                )
-            connection.commit()
-            print(f'LiquidityPoolAddress: {LiquidityPoolAddress}, liquidity: {liquidity}')
-    else:
-        print(f'LiquidityPoolAddress: {LiquidityPoolAddress}, status code: {response.status_code}')
+            self.cursor.execute("""
+                REPLACE INTO LiquidityData (LiquidityPoolAddress, liquidity) 
+                VALUES (?, ?)
+            """, (liquidity_pool_address, liquidity))
+            self.connection.commit()
+            print(f'LiquidityPoolAddress: {liquidity_pool_address}, liquidity: {liquidity}')
+        else:
+            print(f'LiquidityPoolAddress: {liquidity_pool_address}, status code: {response.status_code}')
 
-for LiquidityPoolAddress in distinct_addresses:
-    extract_liquidity_info(LiquidityPoolAddress)
-    time.sleep(1)
+    def retrieve_liquidity_information(self):
+        distinct_addresses = self.fetch_distinct_addresses()
+        for liquidity_pool_address in distinct_addresses:
+            self.extract_liquidity_info(liquidity_pool_address)
+            time.sleep(1)
+
+    def run(self):
+        while True:
+            self.retrieve_liquidity_information()
+            print(f"Waiting for {self.delay} seconds before next update...")
+            time.sleep(self.delay)
+
+# usage:
+if __name__ == "__main__":
+    liquidity_retriever = LiquidityInfoRetriever(delay=10)  # Set delay to x seconds)
+    liquidity_retriever.run()
